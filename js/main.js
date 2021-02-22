24ac08fdb9485f7ffadc6ccaacd06313ccdaaeeb
sean845 <74813186+sean845@users.noreply.github.com>
2021-02-22 14:39:36 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+    const menuIcon = document.querySelector('.hamburger-menu');
+    
+    const navbar = document.querySelector('.navbar');
+    
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change');
+    });
+
+}
