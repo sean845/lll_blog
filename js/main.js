d0dabf6ce3085a9619eede946ee12c66ff9752c5
sean845 <74813186+sean845@users.noreply.github.com>
2021-02-28 09:33:21 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@
 
 {
     const menuIcon = document.querySelector('.hamburger-menu');
-    
     const navbar = document.querySelector('.navbar');
-    
     menuIcon.addEventListener('click', () => {
         navbar.classList.toggle('change');
     });
+
+    const fadeIn = document.getElementById('img');
+    fadeIn.animate([{opacity: '0'}, {opacity: '2'}], 1500);
 
 }
