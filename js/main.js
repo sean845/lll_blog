464fab69f6cde3e0b03e5ebb4f8033e4aa88a3ed
sean845 <74813186+sean845@users.noreply.github.com>
2021-03-20 09:23:52 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,4 +10,11 @@
     const fadeIn = document.getElementById('img');
     fadeIn.animate([{opacity: '0'}, {opacity: '2'}], 1500);
 
+    const contact = document.getElementById('contact');
+    const sns = document.getElementById('sns');
+
+    contact.addEventListener('click', () => {
+        sns.classList.toggle('menu-bar');
+    });
+
 }
